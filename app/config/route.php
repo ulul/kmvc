410c76152465fe08a11a7a50549fe7b4c1a0373a
ulul <hudanuzulul@gmail.com>
2016-05-02 08:43:41 +0700

    first commit

---
+++ app/config/route.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * route  yaitu untuk memanipulasi url tanpa htaccess contoh kita akan megakses controller/index
+ * $route["alias"] = "controller/index";
+ * Akses url yaitu "http://localhost/root_dir/alias" / http://localhost/alias
+ * jika anda menggunakan parameter url jangan menggunakan uri routing karena fitur masih belum sempurna
+ * */
+
+
+$route["app.mvc"] = "controller/index";
+
+// End Of file route.php
